6f0fdecb750e4ab3d956130acb0b8ab242f90579
Connor Riley <criley38@gatech.edu>
2024-04-29 12:08:59 -0400

    testing 1-depth delays

--- test/test_example.py
+++ test/test_example.py
@@ -16,23 +16,13 @@ def test_one(celery_app, celery_worker):
     assert result == 20
 
 
-# this fails -- why?
-# Only works this way
-# @pytest.mark.celery(
-#     broker_url="redis://localhost:6379", result_backend="redis://localhost:6379"
-# )
-# # Worker initialized per test
-# @pytest.mark.usefixtures("celery_worker")
-# def test_multiply_delay() -> None:
-#     assert src.celery_tasks.multiply.delay(4, 5).get(timeout=10) == 20
+def test_multiply_delay(celery_app, celery_worker) -> None:
+    assert src.celery_tasks.multiply.delay(4, 5).get(timeout=10) == 20
 
 
-# def test_multiply(celery_config, celery_worker):
-#     assert src.celery_tasks.multiply(4, 5) == 20
+def test_multiply(celery_config, celery_worker):
+    assert src.celery_tasks.multiply(4, 5) == 20
 
 
-# # this fails as well
-# @pytest.mark.usefixtures("celery_session_app")
-# @pytest.mark.usefixtures("celery_session_worker")
-# def test_hellothenmultiply(celery_config, celery_session_app, celery_session_worker):
-#     assert src.celery_tasks.hellothenmultiply(4, 5).get(timeout=10) == 20
+def test_hellothenmultiply(celery_app, celery_worker):
+    assert src.celery_tasks.hellothenmultiply(4, 5).get(timeout=10) == 20
